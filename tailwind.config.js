4de44b3045970183752999b5e349ec7bc944b07a
rickyfelix19 <rickyfelix19@outlook.com>
2023-01-14 16:42:48 +1100

    fix eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue, js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: "#f36163",
-      },
-    },
+        primary: '#f36163'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
